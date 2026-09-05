0a75846a6f1cd52a77a306d55724f382d77d6388
mathurrakshita2005 <mathurrakshita2005@gmail.com>
2026-09-05 17:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
-};
+const prices = document.querySelectorAll(".prices");
+	let total =0;
+
+	prices.forEach(price) => {
+		total += Number(price.textContent);
+	});
+
+	const row = document.createElement("tr");
+	const cell = document.createElement("td");
+
+	cell.colspam=2;
+	cell.textContent=`Total Price: Rs ${total}`;
+
+	row.appendChild(cell);
+
+	document.querySelector("table").appendChild(row);
 
 getSumBtn.addEventListener("click", getSum);
 
